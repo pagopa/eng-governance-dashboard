d9acba4e1547c512f03f153907da22f122bf54e0
maurodandrea <ing.mauro.dandrea@gmail.com>
2025-09-17 17:21:12 +0200

    leng

--- src/scripts/awscli_export_aws_dashboard_health_loganalytics_multiaccount.py
+++ src/scripts/awscli_export_aws_dashboard_health_loganalytics_multiaccount.py
@@ -41,7 +41,8 @@ def post_to_log_analytics(workspace_id, key, log_type, body):
     rfc1123date = datetime.now(timezone.utc).strftime('%a, %d %b %Y %H:%M:%S GMT')
     resource = '/api/logs'
     content_type = 'application/json'
-    content_length = len(body_json)
+    #content_length = len(body_json)
+    content_length = len(body_json.encode('utf-8'))
     #signature = build_signature(workspace_id, key, rfc1123date, content_length, 'POST', content_type, resource)
     signature = build_signature(
     workspace_id=workspace_id,
